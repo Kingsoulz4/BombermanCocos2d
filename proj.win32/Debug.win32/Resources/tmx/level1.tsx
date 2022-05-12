50d9ef87938881eb9056b85e1fbfd1e017375c7b
Dat Hoang <42060490+Kingsoulz4@users.noreply.github.com>
2022-05-12 17:28:29 +0700

    win32 update

---
+++ proj.win32/Debug.win32/Resources/tmx/level1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="level1" tilewidth="32" tileheight="32" tilecount="56" columns="8">
+ <image source="../level/level1.png" width="256" height="240"/>
+</tileset>

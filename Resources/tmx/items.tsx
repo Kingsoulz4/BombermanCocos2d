99141ff4396d9bdbebf980ed1efcfe32e19e8c29
Dat Hoang <42060490+Kingsoulz4@users.noreply.github.com>
2022-05-25 16:25:49 +0700

    update res

---
+++ Resources/tmx/items.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="items" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="items/items.png" width="256" height="64"/>
+ <tile id="0" type="301">
+  <properties>
+   <property name="itemType" type="int" value="301"/>
+  </properties>
+ </tile>
+ <tile id="1" type="302">
+  <properties>
+   <property name="itemType" type="int" value="302"/>
+  </properties>
+ </tile>
+ <tile id="2" type="303">
+  <properties>
+   <property name="itemType" type="int" value="303"/>
+  </properties>
+ </tile>
+ <tile id="3" type="304">
+  <properties>
+   <property name="itemType" type="int" value="304"/>
+  </properties>
+ </tile>
+ <tile id="6" type="305">
+  <properties>
+   <property name="itemType" type="int" value="305"/>
+  </properties>
+  <animation>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="7" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="7" type="305"/>
+ <tile id="8" type="307">
+  <properties>
+   <property name="itemType" type="int" value="307"/>
+  </properties>
+ </tile>
+ <tile id="10" type="306">
+  <properties>
+   <property name="itemType" type="int" value="306"/>
+  </properties>
+ </tile>
+</tileset>

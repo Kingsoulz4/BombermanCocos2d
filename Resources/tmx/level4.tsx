0b9868b136379b47b1711ab02ec87fa49011f905
Dat Hoang <42060490+Kingsoulz4@users.noreply.github.com>
2022-05-27 15:46:42 +0700

    update resource

---
+++ Resources/tmx/level4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="level4" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="level/level4.png" width="256" height="208"/>
+</tileset>
